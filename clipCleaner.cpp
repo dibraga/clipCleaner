cbe04f1fa9faf99bd17e1e9c6a48e845b7bb7510
DiBraga <diogo199915@gmail.com>
2025-03-28 18:37:49 +0000

    Fix Icon

--- clipCleaner.cpp
+++ clipCleaner.cpp
@@ -1,8 +1,10 @@
 #include <windows.h>
 #include <shellapi.h>
+#include "resource.h"
 
 #define TRAY_ICON_ID 1
 #define WM_TRAYICON (WM_USER + 1)
+#define ID_TRAY_EXIT 1001
 
 void ClearClipboard() {
     if (OpenClipboard(NULL)) {
@@ -11,11 +13,28 @@ void ClearClipboard() {
     }
 }
 
+void ShowContextMenu(HWND hwnd) {
+    POINT pt;
+    GetCursorPos(&pt);
+    HMENU hMenu = CreatePopupMenu();
+    AppendMenu(hMenu, MF_STRING, ID_TRAY_EXIT, "Exit");
+    SetForegroundWindow(hwnd);
+    TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hwnd, NULL);
+    DestroyMenu(hMenu);
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_TRAYICON:
             if (lParam == WM_LBUTTONUP) {
                 ClearClipboard();
+            } else if (lParam == WM_RBUTTONUP) {
+                ShowContextMenu(hwnd);
+            }
+            break;
+        case WM_COMMAND:
+            if (LOWORD(wParam) == ID_TRAY_EXIT) {
+                PostMessage(hwnd, WM_CLOSE, 0, 0);
             }
             break;
         case WM_DESTROY:
@@ -43,7 +62,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     nid.uID = TRAY_ICON_ID;
     nid.uFlags = NIF_ICON | NIF_MESSAGE;
     nid.uCallbackMessage = WM_TRAYICON;
-    nid.hIcon = LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
+    nid.hIcon = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
     Shell_NotifyIcon(NIM_ADD, &nid);
     
     SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)&nid);
